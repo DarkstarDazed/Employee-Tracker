7da80bd96be576b4a83d07ca96c9d8937d664735
mediazjr <mediazjr@gmail.com>
2022-01-03 18:30:13 -0800

    added starting prompt and if/else statement to shuffle through the choices in the prompt

--- index.js
+++ index.js
@@ -23,5 +23,39 @@ db.connect((err) => {
 
 
 function startPrompt() {
-    
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "menu",
+            message: "What would you like to do?",
+            choices: [
+              "View all departments",
+              "View all roles",
+              "View all employees",
+              "Add a department",
+              "Add a role",
+              "Add an employee",
+              "Update an employee role",
+            ],
+        }
+    ])
+
+    .then((answers) => {
+        if (answers.menu === "View all departments") {
+            viewDepartments();
+        } else if (answers.menu === "View all roles") {
+            viewRoles();
+          } else if (answers.menu === "View all employees") {
+            viewEmployees();
+          } else if (answers.menu === "Add a department") {
+            addDepartment();
+          } else if (answers.menu === "Add a role") {
+            addRole();
+          } else if (answers.menu === "Add an employee") {
+            addEmployee();
+          } else {
+            updateEmployee();
+          }
+    });
 }
